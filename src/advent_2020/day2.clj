6b5659fca3b6b1ce695df435b2513061a15e9fac
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-02 18:01:25 +0000

    day 2 part 2

--- src/advent_2020/day2.clj
+++ src/advent_2020/day2.clj
@@ -1,11 +1,11 @@
 (ns advent-2020.day2
   (:require [clojure.string :as s]))
 
-(defn- get-coords []
+(defn- get-coords [path]
   (let [parse-fn (fn [line]
                    (let [[first second] (s/split line #" ")]
                      [first (Integer/parseInt second)]))]
-    (map parse-fn (s/split-lines (slurp "resources/coords.txt")))))
+    (map parse-fn (s/split-lines (slurp path)))))
 
 (defn get-total-for-direction [direction coords]
   (->> (filter #(= (first %) direction) coords)
@@ -13,21 +13,44 @@
        (reduce +)))
 
 (defn part1 []
-  (let [coords (get-coords)
+  (let [coords (get-coords "resources/coords.txt")
         horizontal (get-total-for-direction "forward" coords)
         up (* -1 (get-total-for-direction "up" coords))
         down (get-total-for-direction "down" coords)
         vertical (+ up down)]
     (* horizontal vertical)))
 
-(comment
-  (reduce + (map second (filter #(= (first %) "forward") (get-coords))))
+(defn part2 []
+  (let [final-pos
+        (->>
+          coords
+          (reduce (fn [vals item]
+                    (let [previous (last vals)
+                          last-aim (if previous (last previous) 0)
+                          direction-value (second item)
+                          aim (case (first item)
+                                "up" (- last-aim direction-value)
+                                "down" (+ last-aim direction-value)
+                                last-aim)
+                          result  (conj item aim)]
+                      (conj vals result)))
+                  [])
+          (filter #(= "forward" (first %)))
+          (map #(let [direction-value (second %)
+                      aim (last %)
+                      x direction-value
+                      y (* aim direction-value)]
+                  [x y]))
+          (reduce (fn [vals item]
+                    [(+ (first vals) (first item))
+                     (+ (second vals) (second item))])
+                  [0 0]))
+        [x y] final-pos]
+    (* x y)))
 
-  (let [coords (get-coords)
-        horizontal (get-total-for-direction "forward" coords)
-        up (* -1 (get-total-for-direction "up" coords))
-        down (get-total-for-direction "down" coords)
-        vertical (+ up down)]
-    (* horizontal vertical)))
+
+
+
+(comment)
 
 
