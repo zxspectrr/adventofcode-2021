b87fdc0212f4721e5fd350774dfa3135b0bc5603
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-02 18:27:51 +0000

    refactor part 1

--- src/advent_2021/day2.clj
+++ src/advent_2021/day2.clj
@@ -1,10 +1,10 @@
 (ns advent-2021.day2
   (:require [clojure.string :as s]))
 
-(defn- get-coords [path]
+(defn- get-steps [path]
   (let [parse-fn (fn [line]
-                   (let [[first second] (s/split line #" ")]
-                     [first (Integer/parseInt second)]))]
+                   (let [[direction distance] (s/split line #" ")]
+                     [(keyword direction) (Integer/parseInt distance)]))]
     (map parse-fn (s/split-lines (slurp path)))))
 
 (defn get-total-for-direction [direction coords]
@@ -12,43 +12,62 @@
        (map second)
        (reduce +)))
 
+(defn answer [coords]
+  (apply * coords))
+
 (defn part1 []
-  (let [coords (get-coords "resources/steps.txt")
-        horizontal (get-total-for-direction "forward" coords)
-        up (* -1 (get-total-for-direction "up" coords))
-        down (get-total-for-direction "down" coords)
-        vertical (+ up down)]
-    (* horizontal vertical)))
+  (answer
+    (reduce (fn [[x y] [direction distance]]
+              (case direction
+                :forward [(+ x distance) y]
+                :up [x (- y distance)]
+                :down [x (+ y distance)]))
+            [0 0] steps)))
+;
+;(defn part2 []
+;  (let [final-pos
+;        (->>
+;          (get-steps "resources/steps.txt")
+;          (reduce (fn [vals item]
+;                    (let [_  previous (last vals)
+;                          last-aim (if previous (last previous) 0)
+;                          direction-value (second item)
+;                          aim (case (first item)
+;                                :up (- last-aim direction-value)
+;                                :down (+ last-aim direction-value)
+;                                last-aim)
+;                          result  (conj item aim)]
+;                      (conj vals result)))
+;                  [])
+;          (filter #(= :forward (first %)))
+;          (map #(let [direction-value (second %)
+;                      aim (last %)
+;                      x direction-value
+;                      y (* aim direction-value)]
+;                  [x y]))
+;          (reduce (fn [vals item]
+;                    [(+ (first vals) (first item))
+;                     (+ (second vals) (second item))])
+;                  [0 0]))
+;        [x y] final-pos]
+;    (* x y)))
 
-(defn part2 []
-  (let [final-pos
-        (->>
-          coords
-          (reduce (fn [vals item]
-                    (let [previous (last vals)
-                          last-aim (if previous (last previous) 0)
-                          direction-value (second item)
-                          aim (case (first item)
-                                "up" (- last-aim direction-value)
-                                "down" (+ last-aim direction-value)
-                                last-aim)
-                          result  (conj item aim)]
-                      (conj vals result)))
-                  [])
-          (filter #(= "forward" (first %)))
-          (map #(let [direction-value (second %)
-                      aim (last %)
-                      x direction-value
-                      y (* aim direction-value)]
-                  [x y]))
-          (reduce (fn [vals item]
-                    [(+ (first vals) (first item))
-                     (+ (second vals) (second item))])
-                  [0 0]))
-        [x y] final-pos]
-    (* x y)))
+(comment
 
 
-(comment)
+  (part1)
+  (print small-steps)
+  (reduce (fn [[x y] [direction distance]]
+            (println distance)
+            (println direction)
+            (case direction
+              :forward [(+ x distance) y]
+              :up [x (- y distance)]
+              :down [x (+ y distance)]))
+          [0 0] small-steps)
+
+  (print steps)
+  (def steps (get-steps "resources/steps.txt"))
+  (def small-steps (get-steps "resources/small-steps.txt")))
 
 
