8a6567b4f202b3fa69e756aba389b852e7c4f399
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-02 15:12:31 +0000

    Day 2 part 1

--- src/advent_2020/day2.clj
+++ src/advent_2020/day2.clj
@@ -1,17 +1,33 @@
 (ns advent-2020.day2
   (:require [clojure.string :as s]))
 
-(comment
-  (map
-    #(map (fn [item] (do [(first item) (second item)])) (s/split % #" "))
-    (s/split-lines (slurp "resources/coords.txt"))))
-
 (defn- get-coords []
   (let [parse-fn (fn [line]
                    (let [[first second] (s/split line #" ")]
                      [first (Integer/parseInt second)]))]
     (map parse-fn (s/split-lines (slurp "resources/coords.txt")))))
 
+(defn get-total-for-direction [direction coords]
+  (->> (filter #(= (first %) direction) coords)
+       (map second)
+       (reduce +)))
+
+(defn part1 []
+  (let [coords (get-coords)
+        horizontal (get-total-for-direction "forward" coords)
+        up (* -1 (get-total-for-direction "up" coords))
+        down (get-total-for-direction "down" coords)
+        vertical (+ up down)]
+    (* horizontal vertical)))
+
 (comment
-  (get-coords))
+  (reduce + (map second (filter #(= (first %) "forward") (get-coords))))
+
+  (let [coords (get-coords)
+        horizontal (get-total-for-direction "forward" coords)
+        up (* -1 (get-total-for-direction "up" coords))
+        down (get-total-for-direction "down" coords)
+        vertical (+ up down)]
+    (* horizontal vertical)))
+
 
