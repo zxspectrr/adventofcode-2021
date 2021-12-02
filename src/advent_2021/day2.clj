46c415d1b9f6c337d7b0845b5cdcf5115ad7b41d
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-02 21:50:59 +0000

    refactor

--- src/advent_2021/day2.clj
+++ src/advent_2021/day2.clj
@@ -1,31 +1,36 @@
 (ns advent-2021.day2
   (:require [clojure.string :as s]))
 
+(defn- parse [line]
+  (let [[direction distance] (s/split line #" ")]
+    [(keyword direction) (Integer/parseInt distance)]))
+
 (defn- get-steps [path]
-  (let [parse-fn (fn [line]
-                   (let [[direction distance] (s/split line #" ")]
-                     [(keyword direction) (Integer/parseInt distance)]))]
-    (map parse-fn (s/split-lines (slurp path)))))
+  (map parse (s/split-lines (slurp path))))
 
-(defn steps (get-steps "resources/steps.txt"))
-
-(defn multiply-coords [[x y]]
-  (* x y))
+(def steps (get-steps "resources/steps.txt"))
 
 (defn part1 []
-  (multiply-coords
+  (->>
+    steps
     (reduce (fn [[x y] [direction distance]]
               (case direction
                 :forward [(+ x distance) y]
                 :up [x (- y distance)]
                 :down [x (+ y distance)]))
-            [0 0] steps)))
+            [0 0])
+    (apply *)))
 
 (defn part2 []
-  (multiply-coords
+  (->>
+    steps
     (reduce (fn [[x y aim] [direction distance]]
               (case direction
                 :forward [(+ x distance) (+ y (* distance aim)) aim]
                 :up [x y (- aim distance)]
                 :down [x y (+ aim distance)]))
-            [0 0 0] steps)))
+            [0 0 0])
+    (take 2)
+    (apply *)))
+
+(comment)
