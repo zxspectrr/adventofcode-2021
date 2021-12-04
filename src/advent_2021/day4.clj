44c728c1059e82206817fe06ac198cd0d0dad668
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-04 20:14:42 +0000

    Load scores and boards

---
+++ src/advent_2021/day4.clj
@@ -0,0 +1,21 @@
+(ns advent-2021.day4
+  (:require [clojure.string :as s]))
+
+(defn expand-board [lines]
+  (map (fn [line]
+         (->> (s/split (s/trim line) #"\s+")
+              (map (fn [string-num]
+                     [(Integer/parseInt string-num) false]))))
+       lines))
+
+(defn load-scores-and-boards []
+  (let [lines (s/split-lines (slurp "resources/small-bingo.txt"))
+        scores (->> (s/split (first lines) #",")
+                    (map #(Integer/parseInt %)))
+        boards (->> (filter #(not= "" %) (rest lines))
+                    (partition 5)
+                    (map expand-board))]
+    {:scores scores :boards boards}))
+
+(comment
+  (load-scores-and-boards))
