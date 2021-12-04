95ae73e164e2d5cd02c5e5bfa4638d1cc091709d
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-04 23:03:41 +0000

    wip

--- src/advent_2021/day4.clj
+++ src/advent_2021/day4.clj
@@ -8,7 +8,7 @@
        lines))
 
 (defn load-scores-and-boards []
-  (let [lines (s/split-lines (slurp "resources/bingo.txt"))
+  (let [lines (s/split-lines (slurp "resources/small-bingo.txt"))
         scores (->> (s/split (first lines) #",")
                     (map #(Integer/parseInt %)))
         boards (->> (filter #(not= "" %) (rest lines))
@@ -42,13 +42,14 @@
 
 (defn run-games [boards scores]
   (loop [bs boards
-         sc scores]
+         sc scores
+         previous-scores []]
     (let [new-score (first sc)
           new-boards (update-boards bs new-score)
           winning-boards (find-winning-boards new-boards)]
       (if (or (= 1 (count sc)) (not (empty? winning-boards)))
-        [winning-boards new-score]
-        (recur new-boards (rest sc))))))
+        [winning-boards new-score (conj previous-scores new-score)]
+        (recur new-boards (rest sc) (conj previous-scores new-score))))))
 
 (defn score-for-board [board winning-score]
   (->> board
@@ -59,13 +60,47 @@
        (* winning-score)))
 
 (defn part1 []
-  (let [[scores boards] (load-scores-and-boards)
+  (let [[scores boards _] (load-scores-and-boards)
         result (run-games boards scores)
         [winning-boards winning-score] result]
     (score-for-board (first winning-boards) winning-score)))
 
 (comment
-  (part1))
+
+  (loop [games boards
+         sc scores]
+    (if (= 1 (count games))
+      (do (println games) (score-for-board (first games) sc))
+      (let [[winning-boards winning-score previous-scores] (run-games boards scores)
+            new-boards (filter #(not= (first winning-boards) %) boards)
+            rest-of-scores (drop (count previous-scores) sc)]
+        (print new-boards)
+        (println winning-score)
+        (println rest-of-scores)
+        (println previous-scores)
+        nil)))
+        ;(recur new-boards rest-of-scores)))
+  ;(recur new-boards rest-of-scores))))
+
+
+  (drop)
+
+  (def winning-boardz (first (run-games boards scores)))
+  (first winning-boardz)
+  (filter #(not= (first winning-boardz) %) boards)
+
+winning-board
+
+  (def list1 [1 2 3 4 5 6])
+  (def list2 [1 2 3 4])
+
+  (drop (count list2) list1)
+
+  (filter #(not (is-present %) = [2 3] %) [[5 6] [1 2] [2 3]])
+
+  (def scores (first (load-scores-and-boards)))
+  (def boards (second (load-scores-and-boards)))
+  (part1)
 
 
 
