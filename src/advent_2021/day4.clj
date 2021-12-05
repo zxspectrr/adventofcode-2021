f23a6023df82fd384ec1fc93372eac7bc5097b2a
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-05 14:40:01 +0000

    wip

--- src/advent_2021/day4.clj
+++ src/advent_2021/day4.clj
@@ -1,14 +1,16 @@
 (ns advent-2021.day4
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.pprint :refer [pprint]]))
 
 (defn expand-board [lines]
-  (map (fn [line]
-         (->> (s/split (s/trim line) #"\s+")
-              (map #(vector (Integer/parseInt %) false))))
-       lines))
+  {:board (map (fn [line]
+                 (->> (s/split (s/trim line) #"\s+")
+                      (map #(vector (Integer/parseInt %) false))))
+           lines)
+   :winner false})
 
 (defn load-scores-and-boards []
-  (let [lines (s/split-lines (slurp "resources/small-bingo.txt"))
+  (let [lines (s/split-lines (slurp "resources/bingo.txt"))
         scores (->> (s/split (first lines) #",")
                     (map #(Integer/parseInt %)))
         boards (->> (filter #(not= "" %) (rest lines))
@@ -34,73 +36,47 @@
 (defn winning-board? [board]
   (or (check-row board) (check-row (pivot board))))
 
-(defn find-winning-boards [boards]
-  (filter winning-board? boards))
+(defn update-board [board score]
+  (->> (mark-board (:board board) score)
+       ((fn [new-board] {:board new-board
+                         :winner (winning-board? new-board)
+                         :score (if (winning-board? new-board) score nil)}))))
 
 (defn update-boards [boards score]
-  (map #(mark-board % score) boards))
+  (map #(update-board % score) boards))
 
-(defn run-games [boards scores]
-  (loop [bs boards
-         sc scores
-         previous-scores []]
-    (let [new-score (first sc)
-          new-boards (update-boards bs new-score)
-          winning-boards (find-winning-boards new-boards)]
-      (if (or (= 1 (count sc)) (not (empty? winning-boards)))
-        [winning-boards new-score (conj previous-scores new-score)]
-        (recur new-boards (rest sc) (conj previous-scores new-score))))))
+(defn run-game [p-boards p-scores]
+  (loop [boards p-boards
+         scores p-scores
+         winners []]
+    (let [score (first scores)
+          updated-boards (update-boards boards score)
+          new-winners (filter #(:winner %) updated-boards)
+          remaining-boards (filter #(not (:winner %)) updated-boards)
+          all-winners (concat winners new-winners)]
+      (if (= 1 (count scores))
+        all-winners
+        (recur remaining-boards (rest scores) all-winners)))))
 
-(defn score-for-board [board winning-score]
-  (->> board
+(defn score-for-board [board]
+  (->> (:board board)
        (mapcat #(do %))
        (filter #(false? (second %)))
        (map first)
        (reduce +)
-       (* winning-score)))
+       (* (:score board))))
 
 (defn part1 []
-  (let [[scores boards _] (load-scores-and-boards)
-        result (run-games boards scores)
-        [winning-boards winning-score] result]
-    (score-for-board (first winning-boards) winning-score)))
+  (->> (load-scores-and-boards)
+       ((fn [[scores boards]] (run-game boards scores)))
+       (first)
+       (score-for-board)))
 
-(comment
-
-  (loop [games boards
-         sc scores]
-    (if (= 1 (count games))
-      (do (println games) (score-for-board (first games) sc))
-      (let [[winning-boards winning-score previous-scores] (run-games boards scores)
-            new-boards (filter #(not= (first winning-boards) %) boards)
-            rest-of-scores (drop (count previous-scores) sc)]
-        (print new-boards)
-        (println winning-score)
-        (println rest-of-scores)
-        (println previous-scores)
-        nil)))
-        ;(recur new-boards rest-of-scores)))
-  ;(recur new-boards rest-of-scores))))
-
-
-  (drop)
-
-  (def winning-boardz (first (run-games boards scores)))
-  (first winning-boardz)
-  (filter #(not= (first winning-boardz) %) boards)
-
-winning-board
-
-  (def list1 [1 2 3 4 5 6])
-  (def list2 [1 2 3 4])
-
-  (drop (count list2) list1)
-
-  (filter #(not (is-present %) = [2 3] %) [[5 6] [1 2] [2 3]])
-
-  (def scores (first (load-scores-and-boards)))
-  (def boards (second (load-scores-and-boards)))
-  (part1)
+(defn part2 []
+  (->> (load-scores-and-boards)
+       ((fn [[scores boards]] (run-game boards scores)))
+       (last)
+       (score-for-board)))
 
 
 
