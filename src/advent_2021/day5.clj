84cf8807e3db98d9a665e50ad323563b8d390c54
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-06 00:11:50 +0000

    Find lines for point

--- src/advent_2021/day5.clj
+++ src/advent_2021/day5.clj
@@ -1,16 +1,23 @@
 (ns advent-2021.day5
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [clojure.pprint :refer [pprint]]))
 
 (defn parse-coords [coord-string]
   (->> (s/split coord-string #",")
-       (map (fn [string-num] (Integer/parseInt string-num)))
-       ((fn [[x y]] {:x x :y y}))))
+       (map (fn [string-num] (Integer/parseInt string-num)))))
 
 (defn string-to-line [line-string]
   (->> (s/split line-string #" -> ")
-       ((fn [coords]
-          {:start (parse-coords (first coords))
-           :end (parse-coords (second coords))}))))
+       (map parse-coords)
+       (sort-by first)
+       (sort-by last)
+       ((fn [[start end]]
+          {:start {:x (first start)
+                   :y (last start)}
+           :end {:x (first end)
+                 :y (last end)}}))))
+(comment
+  (load-lines))
 
 (defn straight-line? [{start :start end :end}]
   (or (= (:x start) (:x end)) (= (:y start) (:y end))))
@@ -28,30 +35,19 @@
   (let [{start :start end :end} line
         horizontal-match?
         (and (<= (:x start) (:x point))
-             (> (:x end) (:x point))
+             (>= (:x end) (:x point))
              (= (:y start) (:y point)))
         vertical-match?
         (and (<= (:y start) (:y point))
-             (> (:y end) (:y point))
+             (>= (:y end) (:y point))
              (= (:x start) (:x point)))]
     (or horizontal-match? vertical-match?)))
 
 (defn lines-for-point [point lines]
   (filter #(line-matches-point? % point) lines))
 
-(comment
-
-
-  (line-matches-point? {:start {:x 0, :y 9}
-                        :end {:x 5, :y 9}}
-                       {:x 3 :y 9})
-
-  (line-matches-point? {:start {:x 0, :y 9}, :end {:x 5, :y 9}}
-                       {:x 0 :y 9})
-
-  (->> (straight-lines)
-       (lines-for-point {:x 0 :y 9})
-       (count))
-
-  (load-lines)
-  (s/split "0,9 -> 5,9" #" -> "))
+(defn create-grid []
+  (map (fn [[x y]] {:x x :y y})
+       (for [x (range 0 10)
+             y (range 0 10)]
+         [x y])))
