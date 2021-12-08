cb20df2b2cdf0a026363e79717dcbfd3382ece3e
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-08 17:13:11 +0000

    wip

--- src/advent_2021/day5.clj
+++ src/advent_2021/day5.clj
@@ -11,7 +11,11 @@
 (defn find-line-type [line]
   (let [{start :start end :end} line]
     (if (= (:x start) (:x end)) :vertical
-      (if (= (:y start) (:y end)) :horizontal))))
+      (if (= (:y start) (:y end)) :horizontal
+                                  :diagonal))))
+
+(comment
+  (filter #(= :diagonal (:type %)) (load-lines)))
 
 (defn string-to-line [line-string]
   (->> (s/split line-string #" -> ")
@@ -23,33 +27,52 @@
        (#(assoc % :type (find-line-type %)))))
 
 (defn load-lines []
-  (->> (slurp "resources/lines.txt")
+  (->> (slurp "resources/small-lines.txt")
        (s/split-lines)
        (map #(string-to-line %))))
 
-(defn straight-line? [{type :type}] (some? type))
+(defn straight-line? [{type :type}]
+  (or (= :vertical type) (= :horizontal type)))
 
 (defn straight-lines []
   (->> (load-lines)
        (filter straight-line?)))
 
+(defn horizontal-coords [start end]
+  (map #(do {:x % :y (:y start)})
+       (range (:x start) (inc (:x end)))))
+
+(defn vertical-coords [start end]
+  (map #(do {:x (:x start) :y %})
+       (range (:y start) (inc (:y end)))))
+
+(defn diagonal-coords [start end]
+  (let [start-y (:y start)
+        end-y (:y end)
+        direction (if (pos? (- start-y end-y)) :down :up)]
+    (reduce
+      (fn [all _]
+        (let [last (last all)
+              {x :x y :y} last]
+          (conj all
+                {:x (inc x)
+                 :y (if (= direction :up) (inc y) (dec y))})))
+      [start]
+      (range (:x start) (:x end)))))
+
+
 (defn coords-for-line [line]
   (let [{start :start end :end} line]
     (->> (case (:type line)
-           :horizontal (map #(do {:x % :y (:y start)})
-                            (range (:x start) (inc (:x end))))
-           :vertical (map #(do {:x (:x start) :y %})
-                          (range (:y start) (inc (:y end))))
-           [])
-         (set))))
+           :horizontal (horizontal-coords start end)
+           :vertical (vertical-coords start end)
+           :diagonal (diagonal-coords start end)
+           []))))
+         ;(set))))
 
 (defn find-intersections-for-lines [line1 line2]
   (->> (set/intersection (coords-for-line line1) (coords-for-line line2))
        (#(if (not-empty %) %))))
-
-(comment
-  (->> (straight-lines)
-       (find-intersection-points)))
 
 (defn find-intersection-points [lines]
   (->> (reduce
@@ -82,6 +105,12 @@
 
 (defn part1 []
   (let [lines (straight-lines)
+        points (find-intersection-points lines)]
+    (->> (filter #(>= (count (lines-for-point % lines)) 2) points)
+         (count))))
+
+(defn part2 []
+  (let [lines (load-lines)
         points (find-intersection-points lines)]
     (->> (filter #(>= (count (lines-for-point % lines)) 2) points)
          (count))))
--- test/advent_2021/day5_test.clj
+++ test/advent_2021/day5_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [advent-2021.day5 :refer :all]))
 
+(deftest line-coords
+  (testing "diagonal coords downwards"
+    (let [line {:start {:x 0, :y 8}, :end {:x 8, :y 0}, :type :diagonal}]
+      (is (= [{:x 0 :y 8}
+              {:x 1 :y 7}
+              {:x 2 :y 6}
+              {:x 3 :y 5}
+              {:x 4 :y 4}
+              {:x 5 :y 3}
+              {:x 6 :y 2}
+              {:x 7 :y 1}
+              {:x 8 :y 0}]
+             (coords-for-line line)))))
+
+  (testing "diagonal coords upwards"
+    (let [line {:start {:x 0, :y 0}, :end {:x 8, :y 8}, :type :diagonal}]
+      (is (= [{:x 0 :y 0}
+              {:x 1 :y 1}
+              {:x 2 :y 2}
+              {:x 3 :y 3}
+              {:x 4 :y 4}
+              {:x 5 :y 5}
+              {:x 6 :y 6}
+              {:x 7 :y 7}
+              {:x 8 :y 8}]
+             (coords-for-line line))))))
+
 (deftest line-intersect-points
   (testing "horitonzal line overlap"
     (let [line1 {:start {:x 1 :y 1} :end {:x 3 :y 1} :type :horizontal}
@@ -27,10 +54,10 @@
       (is (= #{{ :x 2 :y 1}}
              (find-intersections-for-lines line1 line2))))))
 
-(deftest all-line-intersections
-  (testing "all intersections"
-    (let [lines (straight-lines)
-          (find-intersection-points lines)])))
+;(deftest all-line-intersections
+;  (testing "all intersections"
+;    (let [lines (straight-lines)
+;          (find-intersection-points lines)])))
 
 (deftest line-matches-point
   (testing "horizontal line match"
