4b93e94960ffc0f5883acc065440338b822dfb2a
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-09 17:06:58 +0000

    Day 7 part 2

--- src/advent_2021/day7.clj
+++ src/advent_2021/day7.clj
@@ -5,24 +5,33 @@
   (-> input-string Double/parseDouble long))
 
 (defn load-values []
-  (->> (slurp "resources/day7.txt")
+  (->> (slurp "resources/day7-small.txt")
        (#(str/split % #","))
        (map parse-long)))
 
-(defn fuel-cost [position destination]
+(defn simple-fuel-cost [position destination]
   (->> (- position destination)
        (Math/abs)))
 
-(defn find-costs [crabs destination]
-  (->> (map #(fuel-cost % destination) crabs)
+(defn compound-fuel-cost [position destination]
+  (->> (simple-fuel-cost position destination)
+       (inc)
+       (range 1)
+       (reduce +)))
+
+(defn find-costs [crabs destination cost-calculator]
+  (->> (map #(cost-calculator % destination) crabs)
        (reduce +)))
 
 (defn find-range [crabs]
   (range (inc (apply max crabs))))
 
-(defn min-cost [crabs]
-  (->> (map #(find-costs crabs %) (find-range crabs))
+(defn min-cost [crabs cost-calculator]
+  (->> (map #(find-costs crabs % cost-calculator) (find-range crabs))
        (apply min)))
 
 (defn part1 []
-  (min-cost (load-values)))
+  (min-cost (load-values) simple-fuel-cost))
+
+(defn part2 []
+  (min-cost (load-values) compound-fuel-cost))
