8bc611b04ba3610f147d217d61a9b3b60e27cd38
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-11 19:58:05 +0000

    Find basins

--- src/advent_2021/day9.clj
+++ src/advent_2021/day9.clj
@@ -5,7 +5,7 @@
   (Integer/parseInt str))
 
 (def input
-  (->> (slurp "resources/day9.txt")
+  (->> (slurp "resources/day9-small.txt")
        (str/split-lines)
        (map #(->> (into [] (map parse-int (re-seq #"\d" %)))))
        (into [])))
@@ -39,3 +39,37 @@
         (filter #(smaller-than-neighbours? % _) _)
         (map #(inc (:height %)) _)
         (reduce + _)))
+
+(def line (second input))
+
+(defn do-find-basin-neighbours [basin-point points]
+  (->> (find-neighbours basin-point points)
+       (filter (fn [p] (< (:height p) 9)))))
+
+(def cached-find-basin-neighbours (memoize do-find-basin-neighbours))
+
+(defn do-find-basin-neighbours [basin-points points]
+  (let [new-points (->> (mapcat #(cached-find-basin-neighbours % points) basin-points)
+                        (set)
+                        (into basin-points))]
+    (if (= (count new-points) (count basin-points))
+      new-points
+      (recur new-points points))))
+
+(def basin-points (first (build-points input)))
+
+(def points (build-points input))
+
+(first points)
+
+(comment
+  (count (do-find-basin-neighbours #{{:height 8, :coords [1 2]}} points))
+
+  (defn find-basin-neighbours [point basin-members points]
+    ;(loop [point point]
+      (let [neighbours (do-find-basin-neighbours point points)]
+        (if (empty? neighbours)
+          basin-members
+          (->> (map #(find-basin-neighbours % basin-members points) neighbours))
+          (recur)))))
+
