88b29475240614f9806daa1e3b9a737f1cd70268
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-11 20:42:21 +0000

    wip

--- src/advent_2021/day9.clj
+++ src/advent_2021/day9.clj
@@ -5,7 +5,7 @@
   (Integer/parseInt str))
 
 (def input
-  (->> (slurp "resources/day9-small.txt")
+  (->> (slurp "resources/day9.txt")
        (str/split-lines)
        (map #(->> (into [] (map parse-int (re-seq #"\d" %)))))
        (into [])))
@@ -42,13 +42,16 @@
 
 (def line (second input))
 
-(defn do-find-basin-neighbours [basin-point points]
+(defn is-top-height? [point]
+  (= (:height point) 9))
+
+(defn do-find-basin-neighbours-neighbours [basin-point points]
   (->> (find-neighbours basin-point points)
-       (filter (fn [p] (< (:height p) 9)))))
+       (remove is-top-height?)))
 
-(def cached-find-basin-neighbours (memoize do-find-basin-neighbours))
+(def cached-find-basin-neighbours (memoize do-find-basin-neighbours-neighbours))
 
-(defn do-find-basin-neighbours [basin-points points]
+(defn build-basin [basin-points points]
   (let [new-points (->> (mapcat #(cached-find-basin-neighbours % points) basin-points)
                         (set)
                         (into basin-points))]
@@ -62,12 +65,45 @@
 
 (first points)
 
+(def filtered-points (remove is-top-height? points))
+
 (comment
-  (count (do-find-basin-neighbours #{{:height 8, :coords [1 2]}} points))
+  (build-basin #{(first filtered-points)} (into #{} (first filtered-points)) points)
+
+  (let [filtered-points (remove is-top-height? points)
+        basins (reduce (fn [basins point]
+                         (conj basins (build-basin #{point} points)))
+                       #{}
+                       filtered-points)]
+    (->> (map count basins)
+         (sort)
+         (reverse)
+         (take 3)
+         (reduce *)))
+
+    ;    basin-counts (reduce count basins)
+    ;basin-counts)
+
+  (count #{{:height 8, :coords [1 2]}
+           {:height 8, :coords [5 2]}
+           {:height 6, :coords [2 3]}
+           {:height 7, :coords [3 3]}
+           {:height 7, :coords [4 2]}
+           {:height 5, :coords [2 2]}
+           {:height 7, :coords [3 1]}
+           {:height 8, :coords [1 4]}
+           {:height 8, :coords [0 3]}
+           {:height 6, :coords [3 2]}
+           {:height 8, :coords [4 1]}
+           {:height 7, :coords [1 3]}
+           {:height 8, :coords [4 3]}
+           {:height 8, :coords [2 1]}})
+
+  (count (build-basin #{{:height 2, :coords [0 0]}} points))
 
   (defn find-basin-neighbours [point basin-members points]
     ;(loop [point point]
-      (let [neighbours (do-find-basin-neighbours point points)]
+      (let [neighbours (build-basin point points)]
         (if (empty? neighbours)
           basin-members
           (->> (map #(find-basin-neighbours % basin-members points) neighbours))
