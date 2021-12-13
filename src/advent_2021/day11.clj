13902573df7dc4b90fa61c0c34383306910c3e31
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-13 16:45:27 +0000

    Refactor

--- src/advent_2021/day11.clj
+++ src/advent_2021/day11.clj
@@ -9,40 +9,37 @@
       (mapv (fn [line] (->> (re-seq #"\d" line)
                             (mapv parse-int))))))
 
-;(defn build-grid []
-;  (for [x (range 0 (count (first lines)))
-;        y (range 0 (count lines))]
-;    {:x x :y y :e (get-in lines [y x])}))
-
 (defn build-grid []
-  (->> (for [x (range 0 (count (first lines)))
-             y (range 0 (count lines))]
-         [[x y] {:coords [x y] :e (get-in lines [y x])}])
-       (into (hash-map))))
+  (for [x (range 0 (count (first lines)))
+        y (range 0 (count lines))]
+    {:x x :y y :e (get-in lines [y x])}))
 
-(defn get-neighbours [[x y] grid]
+;(defn build-grid []
+;  (->> (for [x (range 0 (count (first lines)))
+;             y (range 0 (count lines))]
+;         [[x y] {:coords [x y] :e (get-in lines [y x])}])
+;       (into (hash-map))))
+
+(def grid (build-grid))
+
+(defn find-point [[x y] grid]
+  (first (filter (fn [{gridx :x gridy :y}]
+                   (and (= x gridx) (= y gridy)))
+                 grid)))
+
+
+(defn get-neighbours [{:keys [x y]} grid]
   (let [points [[(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]
                 [(dec x) y] [(inc x) y]
                 [(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]]]
-    (keep grid points)))
+    (keep #(find-point % grid) points)))
 
-(get-neighbours [0 0] grid)
-
-(defn get-neighbour-energies [point]
-  (->> (get-neighbours point)
-       ;(map (fn [[x y]] {:coords [x y] :energy (get-energy [x y])}))
-       (keep)
-       (filter :energy)))
+(get-neighbours (find-point [0 0] grid) grid)
 
 (defn flash? [energy] (> 9 energy))
 
-;(defn point-to-item [[x y]]
-;  {:coords [x y] :energy (get-energy [x y])})
+(defn increment-all [grid]
+  (map (fn [{:keys [e] :as point}]
+         (assoc point :e (inc e))) grid))
 
-(defn increment-all [input]
-  (mapv (fn [line]
-          (mapv inc line)) input))
-
-(def next-line (increment-all lines))
-
-(get-neighbour-energies [0 0])
+(def next-line (increment-all grid))
