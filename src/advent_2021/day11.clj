6535a318120f0adb15d760c938fea5911f230928
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-13 18:19:08 +0000

    wip

--- src/advent_2021/day11.clj
+++ src/advent_2021/day11.clj
@@ -10,24 +10,22 @@
                             (mapv parse-int))))))
 
 (defn build-grid []
-  (for [x (range 0 (count (first lines)))
-        y (range 0 (count lines))]
-    {:x x :y y :e (get-in lines [y x])}))
+  (->> (for [x (range 0 (count (first lines)))
+             y (range 0 (count lines))]
+         [[x y] {:coord [x y] :e (get-in lines [y x])}])
+       (into (hash-map))))
 
 (def grid (build-grid))
 
 (defn find-point [[x y] grid]
-  (first (filter (fn [{gridx :x gridy :y}]
-                   (and (= x gridx) (= y gridy)))
-                 grid)))
+  (get grid [x y]))
 
-(defn get-neighbours [{:keys [x y]} grid]
-  (let [points [[(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]
+(defn get-neighbours [point grid]
+  (let [[x y] (:coord point)
+        points [[(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]
                 [(dec x) y] [(inc x) y]
                 [(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]]]
     (keep #(find-point % grid) points)))
-
-(get-neighbours (find-point [0 0] grid) grid)
 
 (defn flash? [{:keys [e]}] (> e 9))
 
@@ -38,18 +36,28 @@
 (defn find-flashed [grid]
   (filter flash? grid))
 
+(defn increment-points [points grid]
+  (->> (increment-all points)
+       (reduce (fn [grid point]
+                 (assoc grid (:coord point) (:e point)))
+               grid)))
+
 (defn increment-neighbours [point grid]
   (->> (get-neighbours point grid)
-       (increment-all)))
+       (increment-all)
+       (reduce (fn [grid point]
+                 (assoc grid (:coord point) (:e point)))
+               grid)))
 
-(defn update-grid-for-flash-point [point grid]
-  (->> (get-neighbours point grid)
-       (increment-all)))
+(defn process-point [point grid]
+  (if (flash? point)
+    (increment-neighbours point grid)
+    grid))
 
-
-(defn process [points grid]
-  (loop [points points
+(defn process [grid flashpoints]
+  (loop [flashed (filter flash? points)
          grid grid]
+
     (let [flashed (filter flash? points)
           neighbours (map #(increment-neighbours % grid) flashed)])))
 
