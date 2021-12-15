e9670f5ab989c19e9dc555b41882c6901ffe9f53
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-15 15:31:10 +0000

    Attempt to load nodes into a map

--- src/advent_2021/day12.clj
+++ src/advent_2021/day12.clj
@@ -5,10 +5,17 @@
 (def lines (u/read-lines "resources/day12-smallest.txt"))
 
 (defn parse-item [item]
-  (->> (str/split item #"-")
-       ((fn [[f t]] {:from f :to t}))))
+  (str/split item #"-"))
+  ;(->> (str/split item #"-")
+  ;     ((fn [[f t]] {:from f :to t})))
 
 (defn build []
-  (mapv parse-item lines))
+  (->>
+    (mapv parse-item lines)
+    (group-by first)
+    (u/map-vals (fn [v] (->> (map (comp keyword second) v)
+                             (flatten))))
+    (u/map-keys keyword)))
+    ;(map (fn [k v]))))
 
 (build)
--- src/advent_2021/utils.clj
+++ src/advent_2021/utils.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str]))
 
 (defn map-vals [f m] (into {} (map (fn [[k v]] [k (f v)]) m)))
+(defn map-keys [f m] (into {} (map (fn [[k v]] [(f k) v]) m)))
 (defn pivot [dataset] (apply map vector dataset))
 (defn parse-int [str] (Integer/parseInt str))
 (defn read-lines [path]
