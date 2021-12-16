fb1e6426eaf38a5bb14b62bc3ed503dabda047c1
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-16 16:15:02 +0000

    walk using reduce

--- src/advent_2021/day12.clj
+++ src/advent_2021/day12.clj
@@ -5,17 +5,43 @@
 (def lines (u/read-lines "resources/day12-smallest.txt"))
 
 (defn parse-item [item]
-  (str/split item #"-"))
-  ;(->> (str/split item #"-")
-  ;     ((fn [[f t]] {:from f :to t})))
+  (->> (str/split item #"-")
+       (mapv keyword)))
 
-(defn build []
-  (->>
-    (mapv parse-item lines)
-    (group-by first)
-    (u/map-vals (fn [v] (->> (map (comp keyword second) v)
-                             (flatten))))
-    (u/map-keys keyword)))
-    ;(map (fn [k v]))))
+(defn build-nodes []
+  (mapv parse-item lines))
 
-(build)
+(defn find-nodes-from [nodes node]
+  (filter (fn [[a _]] (= node a)) nodes))
+
+(defn find-nodes-to [nodes node]
+  (filter (fn [[_ b]] (= node b)) nodes))
+
+(defn find-start [nodes] (find-nodes-from nodes :start))
+(defn find-end [nodes] (find-nodes-to nodes :end))
+
+(defn multi-pass? [node]
+  (->> (str node) (#(= (str/upper-case %) %))))
+
+(def nodes (build-nodes))
+
+(find-nodes-from nodes :start)
+
+(defn walk [nodes from walked]
+  (reduce (fn [w [f t]]
+            (conj w [f t]))
+          walked
+          (find-nodes-from nodes from)))
+
+(defn start [nodes]
+  (walk nodes :start #{}))
+
+(find-nodes-from nodes :A)
+
+nodes
+
+(filter multi-pass? nodes)
+
+(comment
+  (find-end nodes)
+  (find-start nodes))
