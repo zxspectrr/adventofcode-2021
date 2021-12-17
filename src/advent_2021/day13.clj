afb9aa39af3f904bfab0215cdf996ebd93226da6
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-17 12:55:09 +0000

    part 1 complete

--- src/advent_2021/day13.clj
+++ src/advent_2021/day13.clj
@@ -1,9 +1,10 @@
 (ns advent-2021.day13
   (:require [advent-2021.utils :as u]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def instructions
-  (->> (u/read-lines "resources/day13/small.txt")
+  (->> (u/read-lines "resources/day13/input.txt")
        (split-with #(not (= "" %)))))
 
 (defn parse-point [point-line]
@@ -19,4 +20,30 @@
 (def folds
   (->> (rest (second instructions))
        (map parse-fold)))
-(def points (map parse-point (first instructions)))
+(def points
+  (->> (map parse-point (first instructions)) (set)))
+
+(def fold-axis-map {"y" 1 "x" 0})
+
+(defn beyond-fold? [point [direction position]]
+  (let [axis (get fold-axis-map direction)
+        point-position (get point axis)]
+    (> point-position position)))
+
+(defn fold-point [point [direction position]]
+  (let [axis (get fold-axis-map direction)
+        point-position (get point axis)
+        distance (- point-position position)
+        new-point-position (- point-position (* 2 distance))]
+    (assoc point axis new-point-position)))
+
+(defn apply-fold [points fold]
+  (let [to-fold (set (filter #(beyond-fold? % fold) points))
+        filtered-points (set/difference points to-fold)
+        folded (map #(fold-point % fold) to-fold)]
+     (into filtered-points folded)))
+
+(defn part1 []
+  (->> (apply-fold points (first folds))
+       (count)))
+
