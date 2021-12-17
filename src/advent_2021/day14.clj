63e2e6a6ef236eff667db4c45abe0cdfb0692073
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-17 13:56:59 +0000

    Load rule map

--- src/advent_2021/day14.clj
+++ src/advent_2021/day14.clj
@@ -2,4 +2,19 @@
   (:require [advent-2021.utils :as u]
             [clojure.string :as str]))
 
-()
+(def lines (u/read-lines "resources/day14/small.txt"))
+
+(def template (apply vector (first lines)))
+
+(def rule-map
+  (->> (split-with #(not (= "" %)) lines)
+       (second)
+       (rest)
+       (map #(str/split % #" -> "))
+       (into {})))
+
+(defn get-element [char-arr]
+  (get rule-map (reduce str char-arr)))
+
+(get-element (first (partition 2 1 "NNCB")))
+
