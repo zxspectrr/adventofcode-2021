d8e306c860ff2953a6fa4bdc6e4bbb653cc609c0
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-26 00:19:01 +0000

    Get first type of operator working

--- src/advent_2021/day16.clj
+++ src/advent_2021/day16.clj
@@ -51,59 +51,61 @@
         type (binary-to-number type-bits)]
     {:version version :type type}))
 
+
+(declare parse-packet)
+
+(comment
+ (parse-packet "110100010100101001000100100")
+ (parse-packet "0101001000100100"))
+
 (defn parse-literal [binary]
-               (let [substr (subs binary 5)
+               (let [substr (subs binary 6)
                      literal-chunks (extract-chunks (partition 5 substr))
                      flattened-chunks (flatten literal-chunks)
                      literal-binary (apply str flattened-chunks)
                      bit-length (+ 6 (* 5 (count literal-chunks)))
-                     bits (subs binary 0 bit-length)
                      remainder (subs binary bit-length)]
-                 {:literal (binary-to-number literal-binary)
-                  :bits bits
-                  :remainder remainder}))
+                 {:literal   (binary-to-number literal-binary)
+                  :remainder (if (not= "" remainder) remainder)}))
 
-(comment (parse-literal "000"))
+(defn get-packets [binary]
+  (loop [binary binary
+         packets []]
+    (let [packet (parse-packet binary)
+          {:keys [remainder]} packet
+          result (conj packets packet)]
+      (if (not remainder)
+        result
+        (recur remainder result)))))
 
 (defn parse-packet [binary]
-  (letfn [(parse-literal [binary]
-            (let [substr (subs binary 5)
-                  literal-chunks (extract-chunks (partition 5 substr))
-                  flattened-chunks (flatten literal-chunks)
-                  literal-binary (apply str flattened-chunks)
-                  bit-length (+ 6 (* 5 (count literal-chunks)))
-                  bits (subs binary 0 bit-length)
-                  remainder (subs binary bit-length)]
-              {:literal (binary-to-number literal-binary)
-               :bits bits
-               :remainder remainder}))
-
-          (handle-15-bit [binary packets]
-            (let [length-bits (subs binary 1 16)
+  (letfn [(handle-15-bit [binary]
+            (let [length-bits (subs binary 7 22)
                   sub-packet-length (Integer/parseInt length-bits 2)
-                  child-packets (subs binary 16)
-                  child-packet (parse-packet child-packets)]
-              (parse-packet child-packets)))
+                  packet-binary (subs binary 22 (+ 22 sub-packet-length))]
+              {:packets (get-packets packet-binary)}))
 
           (parse-operator [binary]
-            (let [substr (subs binary 6)
-                  length-type (first substr)
+            (let [length-type (get binary 5)
                   fifteen-bit-length (= \0 length-type)]
               (if fifteen-bit-length
-                (handle-15-bit substr)
+                (handle-15-bit binary)
                 false)))]
 
     (let [header (parse-packet-header binary)
           {:keys [type]} header]
 
       (if (is-literal? type)
-        (parse-literal binary)
-        (assoc header :operator (parse-operator binary))))))
+        (merge header (parse-literal binary))
+        (merge header (parse-operator binary))))))
 
 
 (defn part1 []
-  (->> (hex-to-binary hex)
-       (parse-packet)))
+  (->> (hex-to-binary "38006F45291200")
+       (parse-packet))
+
+
+  true)
 
 (def hex "8A004A801A8002F478")
 (def hex "D2FE28")
@@ -111,11 +113,8 @@
 
 (comment
   (parse-packet "1101000101001010010001001000000000")
-
   (defn split-packets [binary])
-
   (hex-to-binary hex)
-
   (count "101111111000101000"))
 
 
