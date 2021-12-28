0c38e3b12f81cdb6fd560a9a6da9a057b358f599
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-28 16:34:09 +0000

    Add simple numbers

--- src/advent_2021/day18.clj
+++ src/advent_2021/day18.clj
@@ -5,7 +5,13 @@
   (->> (u/read-lines "resources/day18/input.txt")
        (map read-string)))
 
+(def numbers [[1,1] [2,2] [3,3] [4,4]])
 
+(defn add-numbers [numbers]
+  (reduce (fn [acc x]
+            (if acc [acc x]
+                    x)) nil numbers))
 
 (comment
+
   ,)
