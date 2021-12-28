4356a82004252b7c78f0b59bde70ec34d33dc073
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-28 12:21:15 +0000

    wip

--- src/advent_2021/day17.clj
+++ src/advent_2021/day17.clj
@@ -7,3 +7,17 @@
 (def target-y (set (range -5 11)))
 
 (defn in-range? [range value] (some range [value]))
+
+(defn adjust-velocity [[x y]]
+  [(if (> x 0) (dec x) 0)
+   (dec y)])
+
+(defn adjust-position [[px py] [vx vy]]
+  [(+ px vx) (+ py vy)])
+
+(defn step [[position velocity]]
+  (let [new-pos (adjust-position position velocity)
+        new-velocity (adjust-velocity velocity)]
+    [new-pos new-velocity]))
+
+(take 5 (iterate step [[0 0] [7 2]]))
