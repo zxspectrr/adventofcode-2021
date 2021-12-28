a9ad0b0d66c91d63afa516b0b1c2bb76ca0cab67
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-28 14:11:17 +0000

    Part 1 complete!

--- src/advent_2021/day17.clj
+++ src/advent_2021/day17.clj
@@ -3,8 +3,8 @@
 ;(def target [[240 292] [-57 90]])
 (def target [[20 30] [-5 -10]])
 
-(def target-x (set (range 20 31)))
-(def target-y (set (range -10 -4)))
+(def target-x (set (range 240 293)))
+(def target-y (set (range -90 -56)))
 
 (defn in-range-axis? [range value] (some range [value]))
 (defn in-range? [[x y]]
@@ -99,5 +99,5 @@
 
 
   (def target [[20 30] [-5 -10]])
-  ;(def target [[240 292] [-57 90]])
+  ;(def target [[240 292] [-57 -90]])
  ,)
