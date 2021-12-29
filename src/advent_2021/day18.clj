91fec912e431a8f258a38c6876d1213cfca3fb48
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-29 21:10:33 +0000

    Refactor

--- src/advent_2021/day18.clj
+++ src/advent_2021/day18.clj
@@ -9,13 +9,11 @@
 (defn edge? [node]
   (or (nil? node) (zip/end? node)))
 
+(def depth (comp count zip/path))
+
 (defn explodable? [node]
-  (let [parent-count (count (zip/path node))
-        n (zip/node node)]
-    (and (zip/branch? node)
-         (> parent-count 3)
-         (number? (first n))
-         (number? (second n)))))
+  (and (= (depth node) 4)
+       (zip/branch? node)))
 
 (defn find-node [node pred direction]
   (loop [node (direction node)]
@@ -49,8 +47,10 @@
 
 (comment
 
-  (def exp (find-node root explodable? zip/next))
+  (depth exp)
+
   (def root (zip/vector-zip [[[[[9 8],1 ],2 ],3 ],4]))
+  (def exp (find-explodable root))
   (def blank (zip/replace exp 0))
 
   (process [[[[[9 8],1 ],2 ],3 ],4])
