e7d6edce9e9d3aba6581384487858f4e9778bbca
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-29 16:59:09 +0000

    First go at explode

--- src/advent_2021/day18.clj
+++ src/advent_2021/day18.clj
@@ -5,55 +5,66 @@
 (def numbers
   (->> (u/read-lines "resources/day18/input.txt")
        (map read-string)))
-;
+
 ;(def numbers [[1,1] [2,2] [3,3] [4,4] [5,5]])
-;
-;
-;(defn go-in [zipper]
-;  (zip/right (zip/down zipper)))
 
-;(defn check-nodes [zipper]
-;  (let [a (zip/node zipper)
-;        b (zip/node (zip/right zipper))]
-;    (number) [a b]))
+(defn find-explodable-node [root]
+  (loop [[node-value _node-info :as node] root]
+    (cond
+      (zip/end? node) nil
+      (and (zip/branch? node)
+           (number? (first node-value))
+           (number? (second node-value))) node
+      :else (recur (zip/next node)))))
 
-(defn leaf? [[n _]]
-  (when (coll? n)
-    (let [[a b] n]
-      (and (number? a) (number? b)))))
+(defn find-left-num [node]
+  (loop [node node
+         path-count 0]
+    (if (zip/branch? node)
+      (recur (zip/prev node) (inc path-count))
+      [node path-count])))
 
-(def branch? (complement leaf?))
+(defn update-left [node]
+  (let [[ln pc] (find-left-num node)
+        [[a _b]] node
+        updated (zip/edit ln + a)]
+    (->> (iterate zip/next updated)
+         (take (inc pc))
+         (last))))
 
-(defn explode? [[n node-info]]
-  (let [{:keys [pnodes]} node-info
-        parent-count (count pnodes)]
-    (> parent-count 3)))
+(defn find-right-num [node]
+  (let [outer (->> (zip/next node) zip/next zip/next)]
+    (loop [n outer
+           path-count 3]
+      (cond
+        (zip/branch? n) (recur (zip/next n) (inc path-count))
+        (zip/end? n) [nil path-count]
+        :else [n path-count]))))
 
-(defn find-leaf [vec]
-  (->> (zip/vector-zip vec)
-       (iterate zip/next)
-       (take-while branch?)
-       (last)
-       (zip/next)))
+(defn update-right [node]
+  (let [[rn pc] (find-right-num node)
+        [[_a b]] node
+        updated (zip/edit rn + b)]
+    (->> (iterate zip/prev updated)
+         (take (inc pc))
+         (last))))
+
+(defn explode [node]
+  (-> (update-left node)
+      (update-right)
+      (zip/replace 0)
+      (zip/root)))
 
 (comment
 
-  (-> (zip/vector-zip [1 2 3])
-      zip/down
-      zip/right
-      (zip/replace [5 6])
-      zip/root)
+  (def root (zip/vector-zip [[3 [2 [ 1,[ 7,3 ]]]] [ 6,[ 5,[ 4,[ 3,2]]]]]))
 
+  (-> (zip/vector-zip [[[[[9,8],1],2],3],4])
+      (find-explodable-node)
+      (explode))
 
-  (->> (find-leaf [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
-       (explode?))
-
-  (->> (zip/vector-zip [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]])
-       (iterate zip/next)
-       (take 100)
-       (last)
-       (zip/node))
 
   ,)
+
 
 
