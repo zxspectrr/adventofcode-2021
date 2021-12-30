a0dc961530c85c91332c605bb1945adc378f1ba4
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-30 19:45:43 +0000

    Part 2 complete

--- src/advent_2021/day20.clj
+++ src/advent_2021/day20.clj
@@ -1,28 +1,27 @@
 (ns advent-2021.day20
   (:require [advent-2021.utils :as u]))
 
-;(def lines (u/read-lines "resources/day20/input.txt"))
-(def lines (u/read-lines "resources/day20/small.txt"))
+(def lines (u/read-lines "resources/day20/input.txt"))
+;(def lines (u/read-lines "resources/day20/small.txt"))
 
 (def decoder (first lines))
 
 (defn load-image []
-  (mapv (fn [line]
-          (mapv (fn [c] c) line))
-        (drop 2 lines)))
+  {:image (mapv (fn [line]
+                  (mapv (fn [c] c) line))
+                (drop 2 lines))
+   :void \.})
 
-(defn get-pixel [image [x y]]
-  (if-let [p (get-in image [y x])]
-    p
-    \.))
+(defn get-pixel [image [x y] default]
+  (get-in image [y x] default))
 
-(defn find-pixel-plus-neighbours [image [x y]]
+(defn find-pixel-plus-neighbours [image [x y] void]
   (let [adjustments [[-1 -1] [0 -1] [1 -1]
                      [-1 0] [0 0] [1 0]
                      [-1 1] [0 1] [1 1]]]
     (->> (map (fn [[ax ay]]
                 [(+ x ax) (+ y ay)]) adjustments)
-         (map (partial get-pixel image)))))
+         (map #(get-pixel image % void)))))
 
 (def binary-map {\. "0" \# "1"})
 
@@ -31,57 +30,50 @@
 (defn binary-to-long [binary-str]
   (Long/parseLong binary-str 2))
 
-(defn decode-point [image point]
-  (->> (find-pixel-plus-neighbours image point)
+(defn decode-point [image point void]
+  (->> (find-pixel-plus-neighbours image point void)
        (map binary-map)
        (apply str)
        (binary-to-long)
        (decode-number)))
 
-(def flip (partial * -1))
-
 (defn get-coords [image]
   (let [width (count (first image))
-        height (count image)
-        padding 2]
-    (->> (for [x (range (flip padding) (+ padding width))
-               y (range (flip padding) (+ padding height))]
+        height (count image)]
+    (->> (for [x (range -1 (inc width))
+               y (range -1 (inc height))]
            [x y])
          (sort-by second)
-         (partition (+ (* 2 padding) width)))))
+         (partition (+ 2 width)))))
 
-(defn process-line [image line]
-  (mapv (partial decode-point image) line))
+(defn process-line [image line void]
+  (mapv #(decode-point image % void) line))
 
-(defn process-image [image]
-  (->> (get-coords image)
-       (mapv #(process-line image %))))
+(defn process-image [{:keys [image void]}]
+  {:image (->> (get-coords image)
+               (mapv #(process-line image % void)))
+   :void (if (and (= \. void)
+                  (= (first decoder) \#))
+           \#
+           \.)})
 
 (defn print-image [image]
   (doseq [x (map (partial apply str) image)]
     (prn x)))
 
+(defn process-times [n]
+  (-> (iterate process-image (load-image))
+      (nth n)
+      :image
+      flatten
+      frequencies))
+
 (defn part1 []
-  (->> (iterate process-image (load-image))
-       (take 3)
-       last
-       flatten
-       frequencies))
+  (process-times 2))
 
+(defn part2 []
+  (process-times 50))
 
-
-
-(comment
-  (load-image)
-
-  (->> (iterate process-image (load-image))
-       (take 3)
-       last
-       print-image)
-
-  (->> (iterate process-image (load-image))
-       (take 3)
-       last))
 
 
 
