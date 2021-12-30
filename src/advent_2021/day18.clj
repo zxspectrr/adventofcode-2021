6eb8a589658f21f65bed4da2c00685e876effa86
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-30 12:26:12 +0000

    Implement adding snailfish numbers

--- src/advent_2021/day18.clj
+++ src/advent_2021/day18.clj
@@ -61,21 +61,30 @@
     splittable? :>> do-split
     number))
 
-(defn process [number]
+(defn reduce-number [number]
   (reduce (fn [acc x]
             (if (= acc x) (reduced x) x))
           (iterate adjust-number number)))
 
+(defn add-numbers [numbers]
+  (reduce (fn [acc x]
+            (reduce-number
+              (if acc [acc x] x)))
+          nil
+          numbers))
+
 (comment
 
-  (adjust-number
-   (adjust-number
-     (adjust-number
-       (adjust-number [[[[[4, 3], 4], 4], [7, [[8, 4], 9]]], [1, 1]]))))
-
-  (do-split
-   (do-split
-     (find-node (zip/vector-zip [[[[0, 7], 4], [15, [0, 13]]], [1, 1]]) splittable? zip/next)))
+  (add-numbers [[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+                [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+                [[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+                [[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+                [7,[5,[[3,8],[1,4]]]]
+                [[2,[2,2]],[8,[8,1]]]
+                [2,9]
+                [1,[[[9,3],9],[[9,0],[0,7]]]]
+                [[[5,[7,4]],7],1]
+                [[[[4,2],2],6],[8,7]]])
 
   (process [[[[0,7],4],[15,[0,13]]],[1,1]])
 
