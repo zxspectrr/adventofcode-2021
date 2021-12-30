691e8c9e05f19c91e7ecc0902ea9f9c035f5c1ce
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-30 16:46:37 +0000

    wip

--- src/advent_2021/day20.clj
+++ src/advent_2021/day20.clj
@@ -6,7 +6,7 @@
 
 (def decoder (first lines))
 
-(def image
+(defn load-image []
   (mapv (fn [line]
           (mapv (fn [c] c) line))
         (drop 2 lines)))
@@ -31,7 +31,7 @@
 (defn binary-to-long [binary-str]
   (Long/parseLong binary-str 2))
 
-(defn decode-point [point]
+(defn decode-point [image point]
   (->> (find-pixel-plus-neighbours image point)
        (map binary-map)
        (apply str)
@@ -45,27 +45,55 @@
   (let [width (count (first image))]
     (* 3 width)))
 
-(get-image-width image)
+;(defn get-image-height [image]
+;  (let [height (count ())]))
 
 (defn get-coords [image]
   (let [width (count (first image))
         height (count image)]
-    (for [x (range (flip width) (double* width))
-          y (range (flip height) (double* height))]
-      [x y])))
+    (->> (for [x (range -2 (+ 2 width))
+               y (range -2 (+ 2 height))]
+           [x y])
+         (sort-by second)
+         (partition 9))))
 
 (defn process-image [image]
-  (->> (map decode-point (get-coords image))
-       (partition (get-image-width image))))
+  ;(->> (map (partial get-pixel image) (get-coords image))
+  ;     (partition 15))
+  (->> (get-coords image)
+       (mapv (fn [line]
+               (mapv (fn [coord]
+                       (decode-point image coord))
+                     line)))))
+       ;(map (partial decode-point image))))
+       ;(mapv (fn [line]
+       ;        (mapv (fn [x] x) line)))))
+       ;(map (partial apply str))))
 
+(defn print-image [image]
+  (doseq [x (map (partial apply str) image)]
+    (prn x))
+  image)
 
 (comment
 
+  (def image (load-image))
+
+  (get-coords image)
+  (decode-point image [4 2])
+
+  (->> (iterate process-image (load-image))
+       (take 1)
+       (print-image))
+
+  (print-image (load-image))
+  (print-image (process-image (load-image)))
 
   (decode-point [2 2])
   (->> (find-pixel-plus-neighbours image [2 2])
        (map binary-map)
        (apply str)
-       (binary-to-long)))
+       (binary-to-long)
+       (map (partial apply str))))
 
 
