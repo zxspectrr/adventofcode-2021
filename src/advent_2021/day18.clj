6e805caabcf64bccf678864708e3d31a0fe55564
Christopher Paul <christopher.paul@autotrader.co.uk>
2021-12-30 13:39:34 +0000

    Refactor

--- src/advent_2021/day18.clj
+++ src/advent_2021/day18.clj
@@ -91,12 +91,12 @@
        (reduce max)))
 
 (defn get-max-val-for-number [numbers number]
-  (let [other-numbers (remove #(= number %) numbers)]
-    (->> (map #(max-val number %) other-numbers)
+  (let [other-numbers (remove (partial = number) numbers)]
+    (->> (map (partial max-val number) other-numbers)
          (reduce max))))
 
 (defn get-max-val-for-numbers [numbers]
-  (->> (map #(get-max-val-for-number numbers %) numbers)
+  (->> (map (partial get-max-val-for-number numbers) numbers)
        (reduce max)))
 
 (defn part2 []
@@ -104,16 +104,6 @@
 
 
 (comment
-  (def numbers [[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-                [[[5,[2,8]],4],[5,[[9,9],0]]]
-                [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-                [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-                [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-                [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-                [[[[5,4],[7,7]],8],[[8,3],8]]
-                [[9,3],[[9,9],[6,[4,9]]]]
-                [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-                [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]])
   (process [[6,[5,[4,[3,2]]]],1])
   (def root (zip/vector-zip [[6,[5,[4,[3,2]]]],1]))
   (def exp (find-explodable root))
