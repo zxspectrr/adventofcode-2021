ced448cd5c0bf7d87dffd82431be72bc6197bd79
Christopher Paul <christopher.paul@autotrader.co.uk>
2022-01-03 15:04:22 +0000

    wip

--- src/advent_2021/day22.clj
+++ src/advent_2021/day22.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]
             [clojure.set :as set]))
 
-(def lines (u/read-lines "resources/day22/smaller.txt"))
+(def lines (u/read-lines "resources/day22/small.txt"))
 
 (defn parse-coord [line]
   (letfn [(get-coord-range [coord-str]
@@ -20,14 +20,7 @@
                       (mapv get-coord-range))]
       [on-off coords])))
 
-(defn valid-coords [[xs ys zs]]
-  (letfn [(correct-size? [v] (and (>= v -50) (<= v 50)))
-          (in-range? [[min max]] (and (correct-size? min) (correct-size? max)))]
-    (and (in-range? xs) (in-range? ys) (in-range? zs))))
-
-(defn get-input [lines]
-  (->> (mapv parse-coord lines)))
-       ;(filter (fn [[v coords]])))
+(defn get-input [lines] (mapv parse-coord lines))
 
 (defn sub-range [[min max]]
   (let [valid-range (range -50 51)]
@@ -36,39 +29,26 @@
          vec
          sort)))
 
-(defn find-valid-sub-range [[xs ys zs]])
-
-(defn step [[value [xs ys zs]]]
+(defn step [[xs ys zs]]
   (->> (for [x (sub-range xs)
              y (sub-range ys)
              z (sub-range zs)]
-         [[x y z] value])
-       (into {})))
+         [x y z])
+       (into #{})))
 
-(defn do-step [cuboid step-vals]
-  (merge cuboid (step step-vals)))
+(defn do-step [cuboid [on-off step-coovals]]
+  (let [step-coords (step step-coovals)]
+    (if (= 1 on-off)
+      (into cuboid step-coords)
+      (set/difference cuboid step-coords))))
 
 (defn process [steps]
-  (reduce do-step {} steps))
-
-(defn find-on [coord-map]
-  (filter (fn [[_ v]] (= v 1)) coord-map))
-
-;(defn find-on [coord-map]
-;  (let [r (sub-range [-50 50])
-;        valid-coords (->> (for [x r y r z r]
-;                            [x y z])
-;                          set)]
-;    (->> (filter (fn [[_ v]] (= v 1)) coord-map)
-;         set
-;         (set/intersection (valid-coords)))))
+  (reduce do-step #{} steps))
 
 (defn part1 []
   (->> (get-input lines)
-       process)
-       find-on
-       count)
+       process
+       count))
 
 (comment
-
   ,)
